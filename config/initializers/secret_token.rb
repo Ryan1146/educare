4a7bae91926c447e1e1f0c1d01a7200d6c58c8b9
Ryan <hiphubryan@gmail.com>
2014-01-12 10:47:31 +0800

    add bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Educare::Application.config.secret_key_base = 'fa6c075e16308a20341625a61d29f2cb8eb2af1164e0f62f6aba9960e2041c44d7f407522f1c7ca369bf9ebae4ea10975e22bac3c7839f4ad7cd88a0f77d3d3e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Educare::Application.config.session_store :cookie_store, key: '_educare_session'

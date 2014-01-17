b93fcd4f3856846357edfde38ea8453d3ab49f4f
Ryan <hiphubryan@gmail.com>
2014-01-18 01:02:27 +0800

    upload img to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,9 @@ Educare::Application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
-
+  
+  config.action_dispatch.x_sendfile_header = nil
+  
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
@@ -77,6 +79,7 @@ Educare::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
 
 
   config.paperclip_defaults = {
